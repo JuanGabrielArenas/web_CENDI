aff124efb4b8ff1364c1d3b89b3730ce04116f92
JuanGabrielArenas <juanarenas215579@correo.itm.edu.co>
2023-06-09 14:15:39 -0500

    Cooreccion paginas

--- webCENDI/webCENDI/frmPrincipal.Master.designer.cs
+++ webCENDI/webCENDI/frmPrincipal.Master.designer.cs
@@ -141,6 +141,24 @@ namespace webCENDI
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder4;
 
         /// <summary>
+        /// ScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+
+        /// <summary>
+        /// Panel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+
+        /// <summary>
         /// Image1 control.
         /// </summary>
         /// <remarks>
